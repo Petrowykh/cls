70b478533dc1bbbf19d225f63ed0c1fdde957396
a.petrovyh <a..petrovyh@belbohemia.by>
2020-10-06 15:51:30 +0300

    master

--- app/app.py
+++ app/app.py
@@ -1,6 +1,6 @@
 # Новый проект
 
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 
 app = Flask(__name__)
@@ -11,6 +11,12 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     return render_template('index.html')
+
+@app.route('/results', methods=['POST'])
+def render_result():
+    number1 = request.form['number1']
+    number2 = request.form['number2']
+    return "Summa: " + number1 + number2
 
 if __name__== '__main__':
     app.run(debug=True)
